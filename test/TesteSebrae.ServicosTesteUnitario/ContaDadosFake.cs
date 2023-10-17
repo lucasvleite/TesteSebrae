9b7b73726efd69654fe5aba4f1fd7ddf1643fa12
Lucas Vicente Leite <lucasvleite@outlook.com>
2023-10-17 00:16:08 -0300

    Tarefa004 - Criando projeto de Testes unitarios e implementando testes para servico Adiciona, Atualiza e BuscaTodos.

---
+++ test/TesteSebrae.ServicosTesteUnitario/ContaDadosFake.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using TesteSebrae.Dominio;
+
+namespace TesteSebrae.ServicosTesteUnitario
+{
+    public static class ContaDadosFake
+    {
+        public static Conta ContaFake() => Faker.Generate();
+
+        public static IEnumerable<Conta> ContasFake(int quantidade = 10) =>
+            Faker.Generate(quantidade);
+
+        private static Faker<Conta> Faker => new Faker<Conta>()
+            .RuleFor(c => c.Id, Guid.NewGuid())
+            .RuleFor(c => c.Descricao, f => f.Lorem.Word())
+            .RuleFor(c => c.Nome, f => f.Person.FullName);
+    }
+}
---
+++ test/TesteSebrae.ServicosTesteUnitario/ContaServicoTeste.cs
@@ -0,0 +1,74 @@
+﻿using Moq;
+using TesteSebrae.Dominio;
+using TesteSebrae.Infra.Interfaces;
+using TesteSebrae.Servicos;
+
+namespace TesteSebrae.ServicosTesteUnitario
+{
+    public class ContaServicoTeste
+    {
+        public ContaServicoTeste()
+        {
+        }
+
+        [Fact]
+        public async Task Adiciona_ContaValida_CriaId()
+        {
+            // Arrange
+            var contaFake = ContaDadosFake.ContaFake();
+            var repositorioSimulado = new Mock<IContaRepositorio>();
+            repositorioSimulado.Setup(m => m.Adiciona(It.Is<Conta>(c => c.Id != Guid.Empty), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(contaFake);
+
+            var servico = new ContaServico(repositorioSimulado.Object);
+
+            var contaRequisicao = contaFake;
+            contaRequisicao.Id = Guid.Empty;
+
+            // Act
+            var resultado = await servico.Adiciona(contaRequisicao);
+
+            // Assert
+            Assert.False(resultado.Id == Guid.Empty);
+        }
+
+        [Fact]
+        public async Task Atualiza_ContaValida_VinculaIdNaConta()
+        {
+            // Arrange
+            var repositorioSimulado = new Mock<IContaRepositorio>();
+            repositorioSimulado.Setup(m => m.Atualiza(It.IsAny<Conta>(), It.IsAny<CancellationToken>()));
+
+            var servico = new ContaServico(repositorioSimulado.Object);
+
+            var contaRequisicao = ContaDadosFake.ContaFake();
+            contaRequisicao.Id = Guid.Empty;
+            var guid = Guid.NewGuid();
+
+            // Act
+            await servico.Atualiza(guid, contaRequisicao);
+
+            // Assert
+            repositorioSimulado.Verify(v => v.Atualiza(It.Is<Conta>(c => c.Id != Guid.Empty), It.IsAny<CancellationToken>()), Times.Once());
+            Assert.True(contaRequisicao.Id == guid);
+        }
+
+        [Fact]
+        public async Task BuscaTodos_RetornaTodasContas()
+        {
+            // Arrange
+            var resultadoEsperado = ContaDadosFake.ContasFake(20);
+            var repositorioSimulado = new Mock<IContaRepositorio>();
+            repositorioSimulado.Setup(m => m.BuscaTodos(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(resultadoEsperado);
+
+            var servico = new ContaServico(repositorioSimulado.Object);
+
+            // Act
+            var resultado = await servico.BuscaTodos();
+
+            // Assert
+            Assert.True(resultado == resultadoEsperado);
+        }
+    }
+}
